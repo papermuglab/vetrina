100190988820ecea2091bb5274ccf63b3db21d35
Atul Bhalerao <atulya.bhalerao@outlook.com>
2019-03-05 12:21:26 +0530

    about us, directors home page

--- HTML/layout/scripts/jquery.flexslider-min.js
+++ HTML/layout/scripts/jquery.flexslider-min.js
@@ -20,12 +20,12 @@ File: Flexslider Configuration Settings
 $('.basicslider').flexslider({
 	slideshow: true, // Change to "false" to make the slides not slide automatically
 	animation: "slide",
-	animationLoop: false,
+	animationLoop: true,
 	pauseOnHover: true,
-	controlNav: false,
-	directionNav: true,
-	prevText: "<i class='fa fa-angle-left'></i>",
-	nextText: "<i class='fa fa-angle-right'></i>",
+	controlNav: true,
+	directionNav: false,
+	// prevText: "<i class='fa fa-angle-left'></i>",
+	// nextText: "<i class='fa fa-angle-right'></i>",
 	smoothHeight: true,
 	start: function(slider) {
 		$('body').removeClass('loading');
